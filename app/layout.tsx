2900fc39d6cb44d066a35e95862ff76cf429a341
Alvaro Gaona <alvgaona@gmail.com>
2022-11-12 01:21:28 -0300

    Fix showing fallback font on first render

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,10 +1,10 @@
 import '../styles/globals.scss'
-
 import { M_PLUS_Rounded_1c } from '@next/font/google'
 
 const M_PLUS_ROUNDED_1C = M_PLUS_Rounded_1c({
   variable: '--font-mplus',
   weight: ['100', '300', '400', '500', '700', '800', '900'],
+  display: 'fallback',
 })
 
 export default function RootLayout({
