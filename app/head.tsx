6f80dee2580b571d07215a37373d3b5932f99a0b
Alvaro Gaona <alvgaona@gmail.com>
2022-11-13 11:28:13 -0300

    Add my favicon

--- app/head.tsx
+++ app/head.tsx
@@ -5,18 +5,20 @@ const siteMetadata = {
     url: "https://alvgaona.com"
 }
 
-export default async (): Promise<React.ReactNode> => {  
-    return (
-      <>
-        <title>{siteMetadata.title}</title>
-        <meta property="og:title" content={siteMetadata.title} key="og:title"/>
-        <meta property="og:description" content={siteMetadata.description}/>
-        <meta property="og:url" content={siteMetadata.url}/>
-        <meta property="og:type" content="website"/>
-        <meta property="twitter:card" content="summary"/>
-        <meta property="twitter:title" content={siteMetadata.title}/>
-        <meta property="twitter:description" content={siteMetadata.description}/>
-        <meta property="twitter:creator" content={siteMetadata.author}/>
-      </>
-    )
-  }
+const Head = async (): Promise<React.ReactNode> => (
+  <>
+    <title>{siteMetadata.title}</title>
+    <link rel="icon" type="image/svg+xml" href="favicon.svg"/>
+    <meta property="og:title" content={siteMetadata.title} key="og:title"/>
+    <meta property="og:description" content={siteMetadata.description}/>
+    <meta property="og:url" content={siteMetadata.url}/>
+    <meta property="og:type" content="website"/>
+    <meta property="twitter:card" content="summary"/>
+    <meta property="twitter:title" content={siteMetadata.title}/>
+    <meta property="twitter:description" content={siteMetadata.description}/>
+    <meta property="twitter:creator" content={siteMetadata.author}/>
+  </>
+)
+
+export default Head
+
