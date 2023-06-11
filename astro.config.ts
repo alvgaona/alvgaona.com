de0960de9d8c7dd558acc1929c7bb802bdaa8472
Alvaro <alvgaona@gmail.com>
2023-06-11 11:54:23 -0300

    Add vercel+astro adapter

--- astro.config.ts
+++ astro.config.ts
@@ -2,8 +2,12 @@ import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
 import react from "@astrojs/react";
 
+import vercel from "@astrojs/vercel/static";
+
 // https://astro.build/config
 export default defineConfig({
+  output: "static",
+  adapter: vercel(),
   experimental: {
     assets: true
   },
