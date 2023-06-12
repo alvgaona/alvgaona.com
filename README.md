6e7b901b9f1f38e9ab79d7bda97c1a23f686a4a3
Alvaro <alvgaona@gmail.com>
2023-06-11 22:21:44 -0300

    Update theme logic

--- README.md
+++ README.md
@@ -4,7 +4,7 @@
 
 ## Stack
 
-- [Next.js](https://nextjs.org): a React framework built to deliver production applications.
+- [Astro](https://astro.build): a React framework built to deliver production content-driven applications.
 - [React](https://https://reactjs.org/): a JavaScript library for building user interfaces.
 - [Tailwindcss](https://tailwindcss.com/): a CSS library capable of enhancing your styling skills.
 - [Framer Motion](https://www.framer.com/motion/): an animation library that makes animating easy.
---
+++ src/components/Menu.tsx
@@ -0,0 +1,30 @@
+import github from '@assets/github.svg';
+
+const Menu = () => (
+  <div className="flex h-12 flex-row items-center">
+    <ul className="flex h-full flex-row items-center justify-evenly gap-4">
+      <li className="font-mplus hover:underline hover:underline-offset-4">
+        <a href="/">About</a>
+      </li>
+      <li className="font-mplus hover:underline hover:underline-offset-4">
+        <a href="/projects">Projects</a>
+      </li>
+      <li className="font-mplus hover:underline hover:underline-offset-4">
+        <a href="/publications">Publications</a>
+      </li>
+      <li className="font-mplus hover:underline hover:underline-offset-4">
+        <a href="/posts">Posts</a>
+      </li>
+      <li className="font-mplus hover:underline hover:underline-offset-4">
+        <div className="flex flex-row items-center justify-evenly">
+          <img src={github.src} alt="github" width="18" height="18" />
+          <a className="ml-1" href="https://github.com/alvgaona/alvgaona.com">
+            Source
+          </a>
+        </div>
+      </li>
+    </ul>
+  </div>
+);
+
+export default Menu;
