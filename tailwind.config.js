48c3267b6b83abe71cc8df8a7e2c4e7f14d2a139
Alvaro <alvgaona@gmail.com>
2024-01-06 13:01:49 -0300

    feat: update colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,6 +7,14 @@ module.exports = {
       fontFamily: {
         mplus: ['var(--font-mplus)', 'Verdana', 'sans-serif'],
       },
+      colors: {
+        'dark-void': '#151419',
+        'liquid-lava': '#f56e0f',
+        'gluon-gray': '#1b1b1e',
+        'slate-gray': '#262626',
+        'dusty-gray': '#878787',
+        'snow': '#fbfbfb',
+      }
     },
   },
   plugins: [],
