0dc057cd3a587a864774996c3e66ed9ae2dc3552
Alvaro <alvgaona@gmail.com>
2024-08-26 01:24:23 -0300

    fix: add a couple fixes

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -54,6 +54,7 @@ const config = {
         'vesper-dark': '#101010',
         'vesper-gold': '#FFC799',
         'vesper-sky': '#92f3d9',
+        'vitesse-dark': '#121212',
       },
       borderRadius: {
         lg: 'var(--radius)',
@@ -82,16 +83,6 @@ const config = {
     require('@tailwindcss/typography'),
     daisyui,
   ],
-  daisyui: {
-    themes: false, // false: only light + dark | true: all themes | array: specific themes like this ["light", "dark", "cupcake"]
-    darkTheme: 'dark', // name of one of the included themes for dark mode
-    base: false, // applies background color and foreground color for root element by default
-    styled: true, // include daisyUI colors and design decisions for all components
-    utils: true, // adds responsive and modifier utility classes
-    prefix: '', // prefix for daisyUI classnames (components, modifiers and responsive class names. Not colors)
-    logs: true, // Shows info about daisyUI version and used config in the console when building your CSS
-    themeRoot: ':root', // The element that receives theme color CSS variables
-  },
 } satisfies Config;
 
 export default config;
