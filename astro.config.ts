141da0a4bd2c31d761f77d88e682e03f308bd66b
Alvaro <alvgaona@gmail.com>
2024-08-27 01:09:53 -0300

    hotfix: amend date and Astro.url

--- astro.config.ts
+++ astro.config.ts
@@ -9,6 +9,7 @@ import vercel from '@astrojs/vercel/static';
 
 // https://astro.build/config
 export default defineConfig({
+  site: "https://alvgaona.com",
   output: 'static',
   adapter: vercel(),
   integrations: [
