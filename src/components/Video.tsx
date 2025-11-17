f3faeea635615b1d158f8ef55ef55a950a0b0e25
Alvaro Gaona <alvgaona@gmail.com>
2025-11-17 23:09:58 +0100

    fix(react-player): use correct import

--- src/components/Video.tsx
+++ src/components/Video.tsx
@@ -1,9 +1,9 @@
+import ReactPlayer from 'react-player';
 import { AspectRatio } from '@/components/ui/aspect-ratio';
-import ReactPlayer from 'react-player/file';
 
 const Video = ({ url, ratio = 4 / 3 }: { url: string; ratio: number }) => (
     <AspectRatio ratio={ratio}>
-        <ReactPlayer url={url} controls={true} loop={true} playing={true} width="100%" height="100%" />
+        <ReactPlayer src={url} controls={true} loop={true} playing={true} width="100%" height="100%" />
     </AspectRatio>
 );
 
