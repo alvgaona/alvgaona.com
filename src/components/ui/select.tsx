34c19bd35d05ad776414a39ee33059c1c889bd56
Alvaro Gaona <alvgaona@gmail.com>
2026-01-21 00:31:42 +0100

    refactor: more goodies

--- src/components/ui/select.tsx
+++ src/components/ui/select.tsx
@@ -1,5 +1,5 @@
 import * as SelectPrimitive from '@radix-ui/react-select';
-import { Check, ChevronDown, ChevronUp } from 'lucide-react';
+import { Check, CaretDown, CaretUp } from '@phosphor-icons/react';
 import * as React from 'react';
 
 import { cn } from '@/lib/utils';
@@ -24,7 +24,7 @@ const SelectTrigger = React.forwardRef<
     >
         {children}
         <SelectPrimitive.Icon asChild>
-            <ChevronDown className="h-4 w-4 opacity-50" />
+            <CaretDown size={16} className="opacity-50" />
         </SelectPrimitive.Icon>
     </SelectPrimitive.Trigger>
 ));
@@ -39,7 +39,7 @@ const SelectScrollUpButton = React.forwardRef<
         className={cn('flex cursor-default items-center justify-center py-1', className)}
         {...props}
     >
-        <ChevronUp className="h-4 w-4" />
+        <CaretUp size={16} />
     </SelectPrimitive.ScrollUpButton>
 ));
 SelectScrollUpButton.displayName = SelectPrimitive.ScrollUpButton.displayName;
@@ -53,7 +53,7 @@ const SelectScrollDownButton = React.forwardRef<
         className={cn('flex cursor-default items-center justify-center py-1', className)}
         {...props}
     >
-        <ChevronDown className="h-4 w-4" />
+        <CaretDown size={16} />
     </SelectPrimitive.ScrollDownButton>
 ));
 SelectScrollDownButton.displayName = SelectPrimitive.ScrollDownButton.displayName;
@@ -112,7 +112,7 @@ const SelectItem = React.forwardRef<
     >
         <span className="absolute left-2 flex h-3.5 w-3.5 items-center justify-center">
             <SelectPrimitive.ItemIndicator>
-                <Check className="h-4 w-4" />
+                <Check size={16} />
             </SelectPrimitive.ItemIndicator>
         </span>
 
